e2a6bba52ffaa0f4b9976eb6a0842e84fe83459a
Pierre Hugo <avoid3d@gmail.com>
2014-01-30 17:58:49 +0200

    Worked on gumtree backend, scraping categories and scraping locations.

--- moocha/gumtree/gumtree.py
+++ moocha/gumtree/gumtree.py
@@ -3,35 +3,50 @@ import mechanize
 from urllib import urlencode
 from categories import categories
 from advertisement import Advertisement
+import logging
+import json
+logger = logging.getLogger(__name__)
 
 class Gumtree(object):
 	def __init__(self):
+		logger.debug('Creating Gumtree object.')
 		self.home_page_url = 'http://www.gumtree.co.za'
 		self.search_url = 'http://www.gumtree.co.za/search.html'
 		self.categories = categories
 
-	def get_categories_from_website(self):
-		# Get the contents of the home page.
-		home_page_contents = self.get_page_contents(self.home_page_url)
-		soup = BeautifulSoup(home_page_contents)
-		select = soup.find(class_='select')
-		categories = dict() 
-		for link in select.find_all('a'):
-			_id = link.get('data-id')
-			name = link.contents[0]
-			categories[name] = _id
-		return categories
-
+	def search(self, query, category='All categories'):
+		"""Search for ads on gumtree, returning the results as a list
+		of elements of type gumtree.Advertisement.
+		"""
+		logger.debug('Searching for query:%s, category:%s', query, category)
+		category_id = self.categories[category]
+		# Build the search url, combining search terms and categories.
+		url = '?'.join([self.search_url, urlencode({
+			'q': query,
+			'catId': category_id,
+			})])
+		# Get the page's contents.
+		contents = self.get_page_contents(url)
+		# Parse gumtree's response to the mechanize browser.
+		soup = BeautifulSoup(contents)
+		# Get Advertisement objects from the soup.
+		return self.parse_search_results_page(soup)
 
 	def get_page_contents(self, url):
+		"""Fetch the raw contents of a web page (the html text).
+		"""
 		# Instantiate a mechanize browser to open the page, (makes
 		# it easy to handle things like cookies and redirects).
 		browser = mechanize.Browser()
-		# Open the search url with the mechanize browser.
+		# Open the url with the mechanize browser.
 		browser.open(url)
+		# Return the data given the the mechanize browser.
 		return browser.response().read()
 
 	def parse_search_results_page(self, soup):
+		"""Parse the search results page and return a list of gumtree.Advertisement
+		elements as a result.
+		"""
 		body = soup.body
 		viewport = body.find(class_='viewport')
 		containment = body.find(class_='containment')
@@ -57,16 +72,68 @@ class Gumtree(object):
 			))
 		return results
 
+	def get_categories_from_website(self):
+		"""Get a dict associating gumtree's categories with their id
+		from gumtree's homepage.
 
-	def search(self, terms, category='All categories'):
-		# Build the search url, combining search terms and categories.
-		url = '?'.join([self.search_url, urlencode({
-			'q': terms,
-			'catId': self.categories[category],
-			})])
-		# Get the page's contents.
-		contents = self.get_page_contents(url)
-		# Parse gumtree's response to the mechanize browser.
-		soup = BeautifulSoup(contents)
-		# Get Advertisement objects from the soup.
-		return self.parse_search_results_page(soup)
+		This data is still raw and not in the form used by Searcher.
+		"""
+		logger.warn("Getting categories from gumtree's website.")
+		# Get the contents of the home page.
+		home_page_contents = self.get_page_contents(self.home_page_url)
+		logger.info("Fetched gumtree's home page.")
+
+		assert len(home_page_contents), "Got no data fetching gumtree's homepage."
+		soup = BeautifulSoup(home_page_contents)
+
+		categories = dict()	
+
+		script = [script.string for script in soup.find_all('script') if 'CATEGORY_FILTER_JSON' in str(script.string)][0]
+		start_filter = 'CATEGORY_FILTER_JSON = '
+		start = script.find(start_filter)
+		end = script.find('var CATEGORY_FILTER_TITLES')
+		category_json = script[start + len(start_filter):end].replace(';', '')
+		top_category = json.loads(category_json)
+		top_category_name = top_category['localizedName']
+		for middle_category in top_category['children']:
+			middle_category_name = middle_category['localizedName']
+			for bottom_category in middle_category['children']:
+				bottom_category_name = bottom_category['localizedName']
+				id_ = bottom_category['id']
+				category_name = '.'.join([top_category_name, middle_category_name, bottom_category_name])
+				categories[category_name] = id_				
+
+		return categories
+
+	def get_locations_from_website(self):
+		"""Get a dict associating gumtree's locations with their location id.
+		"""
+		logger.warn("Getting locations from gumtree's website.")
+		# Get the contents of the home page.
+		home_page_contents = self.get_page_contents(self.home_page_url)
+		logger.info("Fetched gumtree's home page.")
+		assert len(home_page_contents), "Got no data fetching gumtree's homepage."
+		soup = BeautifulSoup(home_page_contents)
+		location_filter_select = soup.find(class_='location-filter')
+		assert location_filter_select, "Could not find select element on gutree's home page."
+		# Create a dict to store the locations and their ids in.
+		locations = dict() 
+
+		script = [str(script.string) for script in soup.find_all('script') if 'LOCATION_FILTER_JSON' in str(script.string)][0]
+		start_filter = 'LOCATION_FILTER_JSON = '
+		start = script.find(start_filter)		
+		end = script.find(';', start)
+
+		locations_json = script[start + len(start_filter): end]
+
+		top_location = json.loads(locations_json)
+		top_location_name = top_location['localizedName']
+		for middle_location in top_location['children']:
+			middle_location_name = middle_location['localizedName']
+			for bottom_location in middle_location['children']:
+				bottom_location_name = bottom_location['localizedName']
+				id_ = bottom_location['id']
+				location_name = '.'.join([top_location_name, middle_location_name, bottom_location_name])
+				locations[location_name] = id_				
+
+		return locations  
--- moocha/gumtree/test_gumtree.py
+++ moocha/gumtree/test_gumtree.py
@@ -1,10 +1,59 @@
 import unittest
+import gumtree
 from gumtree import Gumtree
+from moocha import config
+from mock import Mock
+from moocha.utils import fuzz
 
 
 class TestGumtree(unittest.TestCase):
 	def setUp(self):
 		self.gumtree = Gumtree()
+
+	def test_search_builds_correct_url(self):
+		# Return an empty string for bs4 to parse.
+		self.gumtree.get_page_contents = Mock()
+		gumtree.BeautifulSoup = Mock
+		self.gumtree.parse_search_results_page = Mock()
+		query = fuzz()
+		category_name = fuzz()
+		category_id = 0
+		self.gumtree.categories = {category_name: category_id}
+		expected_url = 'http://www.gumtree.co.za/search.html?q=%s&catId=0' % query
+		self.gumtree.search(query=query, category=category_name)
+		self.gumtree.get_page_contents.assert_called_with(expected_url)
+
+	def test_search_calls_beautiful_soup(self):
+		page_contents = fuzz()
+		# Return an empty string for bs4 to parse.
+		self.gumtree.get_page_contents = Mock(return_value=page_contents)
+		self.gumtree.parse_search_results_page = Mock()
+		category_name = fuzz()
+		self.gumtree.categories = {category_name: 0}
+		gumtree.BeautifulSoup = Mock()
+		self.gumtree.search(query=fuzz(), category=category_name)
+		gumtree.BeautifulSoup.assert_called_with(page_contents)
+
+	def test_search_calls_parse_search_results(self):
+		category_name = fuzz()
+		self.gumtree.categories = {category_name: 0}
+		self.gumtree.get_page_contents = Mock()	
+		soup = gumtree.BeautifulSoup('')
+		gumtree.BeautifulSoup  = Mock(return_value=soup)
+		self.gumtree.parse_search_results_page = Mock()
+		self.gumtree.search(fuzz(), category_name)
+		self.gumtree.parse_search_results_page.assert_called_with(soup)
+
+	def test_search_returns_parsed_search_results(self):
+		category_name = fuzz()
+		self.gumtree.categories = {category_name: 0}
+		self.gumtree.get_page_contents = Mock()	
+		gumtree.BeautifulSoup  = Mock()
+		expected_search_result = fuzz()
+		self.gumtree.parse_search_results_page = Mock(return_value=expected_search_result)
+		search_result = self.gumtree.search(fuzz(), category_name)
+		self.assertEqual(search_result, expected_search_result)
+
 
 	def test_search(self):
 		# Load the mock search result page.
@@ -12,12 +61,15 @@ class TestGumtree(unittest.TestCase):
 			contents = open('moocha/gumtree/test_pages/search_result.html').read()
 			return contents
 		self.gumtree.get_page_contents = get_page_contents
-		results = self.gumtree.search('hello')
+		results = self.gumtree.search(fuzz())
 
-	@unittest.skip('temp')
+	@unittest.skipIf(not config.get('RUN_INTEGRATION_TESTS'), "Not running integration tests.")
 	def test_get_categories_from_website(self):
-		def get_page_contents(url):
-			contents = open('moocha/gumtree/test_pages/home_page.html')
-		#self.gumtree.get_page_contents = get_page_contents
 		categories = self.gumtree.get_categories_from_website()
-		self.fail()
+		self.assertIsInstance(categories, dict)
+		self.assertIn('All Categories.Property.Short Term', categories)
+
+	@unittest.skipIf(not config.get('RUN_INTEGRATION_TESTS'), "Not running integration tests.")
+	def test_get_locations_from_website(self):
+		locations = self.gumtree.get_locations_from_website()
+		self.assertIn('South Africa.Gauteng.Pretoria / Tshwane', locations)
